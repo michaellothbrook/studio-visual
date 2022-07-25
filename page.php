6df09d3b3e4d686148dae1ebad96b4079ae59c4f
Michael Braga <michaelbraga.dev@gmail.com>
2022-07-25 00:00:01 -0300

    feat(page layout): new structure page

--- page.php
+++ page.php
@@ -11,13 +11,8 @@ get_header();
 the_post();
 ?>
 <div class="row">
-	<div class="col-md-8 order-md-2 col-sm-12">
-		<div id="post-<?php the_ID(); ?>" <?php post_class('content'); ?>>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-			<?php
-			the_content(); ?>
-		</div><!-- /#post-<?php the_ID(); ?> -->
-	</div><!-- /.col -->
+	<?php
+	the_content(); ?>
 </div><!-- /.row -->
 <?php
 get_footer();
